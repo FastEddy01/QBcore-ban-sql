e177aa83967a0a37fc2d272a08a224c1677ab5e0
Ariel11 <77409323+ariel11212321@users.noreply.github.com>
2021-09-21 13:08:02 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# QBcore-ban-sql
-make sql ban offline easy with this script, just edit the config and you can use it. 
+AddEventHandler('QBCore:PlayerJoined',function()
+local src = source
+CreateThread(function()
+Wait(5000)
+local license,steamID,liveid,xblid,discord,playerip
+local playername = GetPlayerName(src)
+
+    	for k,v in ipairs(GetPlayerIdentifiers(src))do
+    		if string.sub(v, 1, string.len("license:")) == "license:" then
+    			license = v
+    		elseif string.sub(v, 1, string.len("steam:")) == "steam:" then
+    			steamID = v
+    		elseif string.sub(v, 1, string.len("live:")) == "live:" then
+    			liveid = v
+    		elseif string.sub(v, 1, string.len("xbl:")) == "xbl:" then
+    			xblid  = v
+    		elseif string.sub(v, 1, string.len("discord:")) == "discord:" then
+    			discord = v
+    		elseif string.sub(v, 1, string.len("ip:")) == "ip:" then
+    			playerip = v
+    		end
+    	end
+
+    	MySQL.Async.fetchAll('SELECT * FROM `baninfo` WHERE `license` = @license', {
+    		['@license'] = license
+    	}, function(data)
+    	local found = false
+    		for i=1, #data, 1 do
+    			if data[i].license == license then
+    				found = true
+    			end
+    		end
+    		if not found then
+    			MySQL.Async.execute('INSERT INTO baninfo (license,identifier,liveid,xblid,discord,playerip,playername) VALUES (@license,@identifier,@liveid,@xblid,@discord,@playerip,@playername)',
+    				{
+    				['@license']    = license,
+    				['@identifier'] = steamID,
+    				['@liveid']     = liveid,
+    				['@xblid']      = xblid,
+    				['@discord']    = discord,
+    				['@playerip']   = playerip,
+    				['@playername'] = playername
+    				},
+    				function ()
+    			end)
+    		else
+    			MySQL.Async.execute('UPDATE `baninfo` SET `identifier` = @identifier, `liveid` = @liveid, `xblid` = @xblid, `discord` = @discord, `playerip` = @playerip, `playername` = @playername WHERE `license` = @license',
+    				{
+    				['@license']    = license,
+    				['@identifier'] = steamID,
+    				['@liveid']     = liveid,
+    				['@xblid']      = xblid,
+    				['@discord']    = discord,
+    				['@playerip']   = playerip,
+    				['@playername'] = playername
+    				},
+    				function ()
+    			end)
+    		end
+    	end)
+    end)
+
+end)
---
+++ config.lua
@@ -0,0 +1,58 @@
+Config                   = {}
+
+
+Config.Lang              = 'en'    --set language - en only right now (support on hebrew later)
+Config.ForceSteam        = true     -- all players will need steam to play on the server if its ture. 
+Config.MultiServerSync   = false   --This will check if a ban is add in the sql all 30 second, use it only if you have more then 1 server (true-false)
+
+
+--WEBHOOK
+Config.EnableDiscordLink = false --Turn this true if you want link the log to a discord (true-false)
+Config.webhookban        = ""
+Config.webhookunban      = ""
+Config.Permission = "admin" -- permission to use commands
+
+
+
+
+
+Config.TextEn = {
+	start         = "BanList and BanListHistory loaded successfully.",
+	starterror    = "ERROR: BanList and BanListHistory failed to load, please retry.",
+	banlistloaded = "BanList loaded successfully.",
+	historyloaded = "BanListHistory loaded successfully.",
+	loaderror     = "ERROR: The BanList failed to load.",
+	cmdban        = "/sqlban (ID) (Duration in days) (Ban reason)",
+	cmdbanoff     = "/sqlbanoffline (Permid) (Duration in days) (Steam name)",
+	cmdhistory    = "/sqlbanhistory (Steam name) or /sqlbanhistory 1,2,2,4......",
+	forcontinu    = " days. To continue, execute /sqlreason [reason]",
+	noreason      = "No reason provided.",
+	during        = " during: ",
+	noresult      = "No results found.",
+	isban         = " was banned",
+	isunban       = " was unbanned",
+	invalidsteam  = "Steam is required to join this server.",
+	invalidid     = "Player ID not found",
+	invalidname   = "The specified name is not valid",
+	invalidtime   = "Invalid ban duration",
+	alreadyban    = " was already banned for : ",
+	yourban       = "You have been banned for: ",
+	yourpermban   = "You have been permanently banned for: ",
+	youban        = "You are banned from this server for: ",
+	forr          = " days. For: ",
+	permban       = " permanently for: ",
+	timeleft      = ". Time remaining: ",
+	toomanyresult = "Too many results, be more specific to shorten the results.",
+	day           = " days ",
+	hour          = " hours ",
+	minute        = " minutes ",
+	by            = "by",
+	ban           = "Ban a player",
+	banoff        = "Ban an offline player",
+	dayhelp       = "Duration (days) of ban",
+	reason        = "Reason for ban",
+	history       = "Shows all previous bans for a certain player",
+	reload        = "Refreshes the ban list and history.",
+	unban         = "Unban a player.",
+	steamname     = "Steam name"
+}
